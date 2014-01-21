4f56168ea483f5e23b7cfdaa683669d4c62d32d9
jwrathall <wrathallj@gmail.com>
2014-01-21 15:34:38 +0100

    release page and some initial ui/layout

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -16,13 +16,28 @@ class AlbumsController < ApplicationController
     xml.remove_namespaces!
     releases = Array.new
     xml.xpath('//release-group-list//release-group').each do |release|
-      r = { 'id' => release.attribute('id').text(),
+      r = { :id => release.attribute('id').text(),
             :title => release.xpath('title').text(),
             :release_date => release.xpath('first-release-date').text()
       }
       releases.push(r)
     end
-    @bandid = releases.sort_by { |hsh| hsh[:release_date] }
+
+    #this is kinda dumb on my end, I couldn't figure out how to concat the two searches! - seriously, like 3 hours burned
+
+    response = conn.get ''+ Settings.musicbrainz_release_query + params['id'] + '&type=soundtrack'
+    xml = Nokogiri::XML(response.body)
+    xml.remove_namespaces!
+
+    xml.xpath('//release-group-list//release-group').each do |release|
+      r = { :id => release.attribute('id').text(),
+            :title => release.xpath('title').text(),
+            :release_date => release.xpath('first-release-date').text()
+      }
+      releases.push(r)
+    end
+
+    @bandid = releases.sort_by { |hsh| hsh[:release_date] }.reverse
 
   end
 
